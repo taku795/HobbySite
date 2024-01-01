a583475d55ea4f166c9442d5818d00c1df324a33
taku795 <takufamilyd4@gmail.com>
2024-01-01 13:48:07 +0900

    投稿時の必須入力追加

--- html/make_page/post.php
+++ html/make_page/post.php
@@ -2,7 +2,7 @@
     require('../../User-Defined/functions.php');
     session_start();
     $error = '';
-    echo $_SESSION['id'];
+
     // 投稿でPOSTされたら
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // タイトル確認
@@ -42,17 +42,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- JQuery -->
+    <script src="../../jQuery-Validation-Engine-master/js/jquery-3.7.1.min.js" type="text/javascript"></script>
+    <!-- 必須入力チェック正規リスト -->
+    <script src="../../jQuery-Validation-Engine-master/js/languages/jquery.validationEngine-ja.js" type="text/javascript" charset="utf-8"></script>
+    <!-- 必須入力チェックエンジン -->
+    <script src="../../jQuery-Validation-Engine-master/js/jquery.validationEngine.js" type="text/javascript" charset="utf-8"></script>
+    <!-- 必須入力チェックスタイルシート -->
+    <link rel="stylesheet" href="../../jQuery-Validation-Engine-master/css/validationEngine.jquery.css" type="text/css"/>
+
     <title>投稿画面</title>
 </head>
 <body>
         
     <h2>趣味に関することを自由に書いてみよう</h2>
     <section class="form_group">
-        <form class="post_form" action='' method='post'>
+        <form class="post_form" action='' method='post' id='post_form'>
             <p>タイトル</p>
-            <input type='text' name='title' placeholder="タイトルを入力">
+            <input type='text' name='title' placeholder="タイトルを入力" class='validate[required]'>
             <p>投稿内容</p>
-            <textarea name="content"></textarea>
+            <textarea name="content" class='validate[required]'></textarea>
             <input type='submit' value='投稿する'>  
         </form>
     </section>
@@ -60,5 +69,13 @@
 </body>
 </html>
 
+
+<script>
+	// 入力チェック用
+	// サイトのドキュメントが準備できたらfunctionを実行
+	$(document).ready(function(){
+		$("#post_form").validationEngine('attach');
+	});
+</script>
 
 
