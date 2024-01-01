5d9f414501bde2542a2f05a463ab5cfcf4e41f82
taku795 <takufamilyd4@gmail.com>
2024-01-01 13:45:48 +0900

    ログイン時のエラー処理追加

--- html/login.php
+++ html/login.php
@@ -1,8 +1,8 @@
 <?php
     // ユーザー定義関数
     require('../User-Defined/functions.php');
-
-    $id_erro = $pw_erro = '';
+	
+    $id_erro = $pw_erro = $erro = '';
     // もし値がポストされていたら処理
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
@@ -23,16 +23,16 @@
         if (isset($pw,$id)) {
             // DB接続
             $pdo = db_connect();
-			
-			// SQLを実行し結果を取得
-			$sql = "select * from users where id = '{$id}';";
-			$result = $pdo -> query($sql);
+			        
+            // SQLを実行し結果を取得
+            $sql = "select * from users where id = '{$id}';";
+            $result = $pdo -> query($sql);
 			
             // ID登録されているか確認
 			if (($result -> rowCount()) == 1) {
 				// 一件だけ該当がある
 				$array = $result -> fetch();
-
+				echo $pw;
 				// IDとパスワードの判定
 				if ($array['ID'] == $id && $array['Password'] == $pw) {
 					// セッションにユーザーIDを登録
@@ -41,6 +41,8 @@
 
 					// ホームに飛ばす
 					header('location:home.php');
+				} else {
+					$erro = 'IDかパスワードが間違っています';
 				}
 			} else if (($result -> rowCount()) == 0) {
 				// 登録されていない場合
@@ -60,8 +62,8 @@
 <!DOCTYPE html>
 <html lang='jp'>
 <head>
-    <!-- JQuery -->
-    <script src="../jQuery-Validation-Engine-master/js/jquery-3.7.1.min.js" type="text/javascript"></script>
+	<!-- JQuery -->
+	<script src="../jQuery-Validation-Engine-master/js/jquery-3.7.1.min.js" type="text/javascript"></script>
     <!-- 必須入力チェック正規リスト -->
     <script src="../jQuery-Validation-Engine-master/js/languages/jquery.validationEngine-ja.js" type="text/javascript" charset="utf-8"></script>
     <!-- 必須入力チェックエンジン -->
@@ -73,36 +75,37 @@
 </head>
 <body>
 
-  <!-- ログインフォーム -->
-  <section class='section_formWrapper'>
-    <!-- ログインフォーム -->
-    <form id='login_form' action='' method='post'>
-      <p>ログインID：</p>
-      <input class='validate[required]' type='text' name='login_id'>
-      <span><?php echo $id_erro; ?></span>
-      <p>パスワード：</p>
-      <input class='validate[required]' type='text' name='login_pw'>
-      <span><?php echo $pw_erro; ?></span>
-      <input class='login_button' type='submit' value='ログイン'>
-    </form>
- 
-    <!-- 新規登録切り替え -->
-    <div class='new_wrapper'>
-      <p>アカウントをお持ちでない方は</p>
-      <a href="index.php">こちら</a>
-    </div>
-    
-  </section>
+	<!-- ログインフォーム -->
+	<section class='section_formWrapper'>
+	<!-- ログインフォーム -->
+	<form id='login_form' action='' method='post'>
+		<span><?php echo $erro; ?></span>
+		<p>ログインID：</p>
+		<input class='validate[required]' type='text' name='login_id'>
+		<span><?php echo $id_erro; ?></span>
+		<p>パスワード：</p>
+		<input class='validate[required]' type='text' name='login_pw'>
+		<span><?php echo $pw_erro; ?></span>
+		<input class='login_button' type='submit' value='ログイン'>
+	</form>
 
-  
+	<!-- 新規登録切り替え -->
+	<div class='new_wrapper'>
+		<p>アカウントをお持ちでない方は</p>
+		<a href="index.php">こちら</a>
+	</div>
+
+	</section>
+
+
 </body>
 </html>
 
 <script>
-  // 入力チェック用
+	// 入力チェック用
 
-  // サイトのドキュメントが準備できたらfunctionを実行
-  $(document).ready(function(){
-    $("#login_form").validationEngine('attach');
-  });
+	// サイトのドキュメントが準備できたらfunctionを実行
+	$(document).ready(function(){
+		$("#login_form").validationEngine('attach');
+	});
 </script>
